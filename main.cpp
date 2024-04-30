53868537692d9e9cc5e6904ec9560153a33b7f18
Coeeen <Krystanjank@gmail.com>
2024-04-30 16:58:31 +0200

    Zmiana Surface na renderara (wydajniejsze i łatwiejsze w uzyciu)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "SDL.h"
-#include "iostream"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "Błąd inicjalizacji SDL: " << SDL_GetError() << std::endl;
         return 1;
@@ -13,28 +12,36 @@ int main(int argc, char* argv[]) {
 
     SDL_Window* okno = SDL_CreateWindow("SGD - PONG", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SZEROKOSC_EKRANU, WYSOKOSC_EKRANU, SDL_WINDOW_SHOWN);
     if (!okno) {
-        std::cerr << "Wystapil blad z wygenerowaniem okna gry" << SDL_GetError() << std::endl;
+        std::cerr << "Wystąpił błąd z wygenerowaniem okna gry: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(okno, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Wystąpił błąd z wygenerowaniem renderera: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(okno);
         SDL_Quit();
         return 1;
     }
 
     bool koniecGry = false;
     SDL_Event event;
-    while(!koniecGry) {
-        while(SDL_PollEvent(&event) !=0) {
-            if(event.type == SDL_QUIT) {
+    while (!koniecGry) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
                 koniecGry = true;
             }
         }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_Surface* tlo =SDL_GetWindowSurface(okno);
-    SDL_FillRect(tlo,NULL,SDL_MapRGB(tlo ->format, 0,0,0));
-
-    SDL_UpdateWindowSurface(okno);
-
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(okno);
     SDL_Quit();
     return 0;
 }
-
