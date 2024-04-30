76777356055b71ffd6c0c1caba8560a642e03100
Coeeen <Krystanjank@gmail.com>
2024-04-30 16:09:33 +0200

    Tworzenie okna i pętli gry

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "SDL.h"
+#include "iostream"
+
+int main(int argc, char* argv[]) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Błąd inicjalizacji SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    const int SZEROKOSC_EKRANU = 800;
+    const int WYSOKOSC_EKRANU = 600;
+
+    SDL_Window* okno = SDL_CreateWindow("SGD - PONG", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SZEROKOSC_EKRANU, WYSOKOSC_EKRANU, SDL_WINDOW_SHOWN);
+    if (!okno) {
+        std::cerr << "Wystapil blad z wygenerowaniem okna gry" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool koniecGry = false;
+    SDL_Event event;
+    while(!koniecGry) {
+        while(SDL_PollEvent(&event) !=0) {
+            if(event.type == SDL_QUIT) {
+                koniecGry = true;
+            }
+        }
+    }
+
+    SDL_Surface* tlo =SDL_GetWindowSurface(okno);
+    SDL_FillRect(tlo,NULL,SDL_MapRGB(tlo ->format, 0,0,0));
+
+    SDL_UpdateWindowSurface(okno);
+
+    SDL_DestroyWindow(okno);
+    SDL_Quit();
+    return 0;
+}
+
