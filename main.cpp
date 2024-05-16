ff9311c335f593dce8108de0af1c2f8736c0bba1
Coeeen <Krystanjank@gmail.com>
2024-05-16 20:51:29 +0200

    Dodanie tła do gry

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include "SDL.h"
 #include <iostream>
 
-const int SZEROKOSC_EKRANU = 800;
-const int WYSOKOSC_EKRANU = 600;
-const int SZEROKOSC_PALETKI = 20;
+const int SZEROKOSC_EKRANU = 1100;
+const int WYSOKOSC_EKRANU = 720;
+const int SZEROKOSC_PALETKI = 5;
 const int WYSOKOSC_PALETKI = 100;
-const int WIELKOSC_PILKI = 5;
-const int PREDKOSC_PILKI = 1;
-const int PREDKOSC_PALETEK = 1;
+const int POZYCJA_PALETEK = 2;
+const int WIELKOSC_PILKI = 10;
+int PREDKOSC_PILKI = 2;
+
+const int TRYB_GRY = 1;
 
  int WYNIK_PALETKA_PIERWSZA = 0;
  int WYNIK_PALETKA_DRUGA = 0;
@@ -35,6 +37,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    SDL_Surface* tloSurface = SDL_LoadBMP("C:/Users/kryst/CLionProjects/GRA/Galeria/tlo1.bmp");
+    if (!tloSurface) {
+        std::cerr << "Wystąpił błąd z ładowaniem obrazu tła: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture* tloTexture = SDL_CreateTextureFromSurface(renderer, tloSurface);
+    SDL_FreeSurface(tloSurface);
+
+
 
     //TWORZENIE OBIEKTÓW
     struct Paletka {
@@ -60,37 +72,34 @@ int main(int argc, char* argv[]) {
 
     struct Pilka {
         SDL_Rect pilka;
-        int wysokoscX;
-        int wysokoscY;
+        int predkoscX;
+        int predkoscY;
 
         Pilka(int x, int y, int predX, int predY) {
             pilka = {x, y, WIELKOSC_PILKI, WIELKOSC_PILKI};
-            wysokoscX = predX;
-            wysokoscY = predY;
+            predkoscX = predX;
+            predkoscY = predY;
         }
 
         void move() {
-            pilka.x += wysokoscX;
-            pilka.y += wysokoscY;
+            pilka.x += predkoscX;
+            pilka.y += predkoscY;
 
             if (pilka.y < 0 || pilka.y > WYSOKOSC_EKRANU - WIELKOSC_PILKI) {
-                wysokoscY = -wysokoscY;
+                predkoscY = -predkoscY;
             }
         }
 
         void reset() {
             pilka.x = SZEROKOSC_EKRANU / 2 - WIELKOSC_PILKI / 2;
             pilka.y = WYSOKOSC_EKRANU / 2 - WIELKOSC_PILKI / 2;
-            wysokoscX = PREDKOSC_PILKI;
-            wysokoscY = PREDKOSC_PILKI;
         }
     };
 
 
     //POŁOŻENIE I TWORZENIE :)
-
-    Paletka paletkaPierwsza(20, WYSOKOSC_EKRANU / 2 - WYSOKOSC_PALETKI);
-    Paletka paletkaDruga(780, WYSOKOSC_EKRANU / 2 - WYSOKOSC_PALETKI);
+    Paletka paletkaPierwsza(10, WYSOKOSC_EKRANU / 2 - WYSOKOSC_PALETKI);
+    Paletka paletkaDruga(1090, WYSOKOSC_EKRANU / 2 - WYSOKOSC_PALETKI);
     Pilka pilka(SZEROKOSC_EKRANU / 2 - WIELKOSC_PILKI / 2, WYSOKOSC_EKRANU / 2 - WIELKOSC_PILKI / 2, PREDKOSC_PILKI, PREDKOSC_PILKI);
 
     bool koniecGry = false;
@@ -105,16 +114,16 @@ int main(int argc, char* argv[]) {
             else if (event.type == SDL_KEYDOWN) {
                 switch (event.key.keysym.sym) {
                     case SDLK_UP:
-                        paletkaPierwsza.wysokosc = -PREDKOSC_PALETEK;
+                        paletkaPierwsza.wysokosc = -POZYCJA_PALETEK;
                     break;
                     case SDLK_DOWN:
-                        paletkaPierwsza.wysokosc = PREDKOSC_PALETEK;
+                        paletkaPierwsza.wysokosc = POZYCJA_PALETEK;
                     break;
                     case SDLK_w:
-                        paletkaDruga.wysokosc = -PREDKOSC_PALETEK;
+                        paletkaDruga.wysokosc = -POZYCJA_PALETEK;
                     break;
                     case SDLK_s:
-                        paletkaDruga.wysokosc = PREDKOSC_PALETEK;
+                        paletkaDruga.wysokosc = POZYCJA_PALETEK;
                     break;
                 }
             }
@@ -134,7 +143,15 @@ int main(int argc, char* argv[]) {
             }
         }
 
-
+        if (TRYB_GRY == 1) {
+            if (pilka.pilka.y < paletkaDruga.prostokat.y) {
+                paletkaDruga.wysokosc = -POZYCJA_PALETEK;
+            } else if (pilka.pilka.y > paletkaDruga.prostokat.y) {
+                paletkaDruga.wysokosc = POZYCJA_PALETEK;
+            } else {
+                paletkaDruga.wysokosc = 0;
+            }
+        }
 
         //KOLOROWANIE NA EKRANIE OBIEKTÓW + DETEKCJA KOLIZJI ODBICIA + WYNIK
         paletkaPierwsza.move();
@@ -147,32 +164,52 @@ int main(int argc, char* argv[]) {
         SDL_Rect paletkaDrugakolizja = paletkaDruga.prostokat;
 
         if(SDL_HasIntersection(&pilkaKolizja, &paletkaPierwszakolizja )|| SDL_HasIntersection(&pilkaKolizja, &paletkaDrugakolizja)) {
-            pilka.wysokoscX = -pilka.wysokoscX;
-        }
+            pilka.predkoscX = -pilka.predkoscX;
 
+            if (pilka.predkoscX > 0) {
+                pilka.predkoscX += 1;
+            } else {
+                pilka.predkoscX -= 1;
+            }
+        }
 
         if (pilka.pilka.x < 0 ) {
             WYNIK_PALETKA_PIERWSZA++;
             std::cout << " Wynik: " << WYNIK_PALETKA_PIERWSZA << " : " << WYNIK_PALETKA_DRUGA << std::endl;
+            pilka.predkoscX = PREDKOSC_PILKI;
             pilka.reset();
         }
         if(pilka.pilka.x > SZEROKOSC_EKRANU) {
             WYNIK_PALETKA_DRUGA++;
             std::cout << " Wynik: " << WYNIK_PALETKA_PIERWSZA << " : " << WYNIK_PALETKA_DRUGA << std::endl;
+            pilka.predkoscX = -PREDKOSC_PILKI;
             pilka.reset();
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        if(WYNIK_PALETKA_PIERWSZA == 3 || WYNIK_PALETKA_DRUGA == 3) {
+            SDL_Quit();
+            std::cout << "GRA ZAKONCZYLA SIE Z WYNIKIEM" << WYNIK_PALETKA_PIERWSZA << " : " << WYNIK_PALETKA_DRUGA << std::endl;
+            return 0;
+        }
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderCopy(renderer, tloTexture, NULL, NULL);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &paletkaPierwsza.prostokat);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
         SDL_RenderFillRect(renderer, &paletkaDruga.prostokat);
 
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
         SDL_RenderFillRect(renderer, &pilka.pilka);
+
+        /*SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDrawLine(renderer, SZEROKOSC_EKRANU / 2, 0, SZEROKOSC_EKRANU / 2, WYSOKOSC_EKRANU);*/
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderDrawLine(renderer, 1, 0, 0, WYSOKOSC_EKRANU);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderDrawLine(renderer, SZEROKOSC_EKRANU - 1, 0, SZEROKOSC_EKRANU - 1, WYSOKOSC_EKRANU);
 
         SDL_RenderPresent(renderer);
 
